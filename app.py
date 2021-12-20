333573e257c9e555cc98b9397ff4f0864bd590f6
Omri Gilhar <gilharomri@gmail.com>
2021-12-20 16:50:49 +0200

    Added Base Postgres support with users tables

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask
+import os
+import psycopg2
 
+# Initiate Flask App
 app = Flask(__name__)
+
+# Getting the DB URL from the local environment variable
+DATABASE_URL = os.getenv('DATABASE_URL', None)
+# Connecting to Postgres DB on Heroku, and set the connection to autocommit
+conn = psycopg2.connect(
+    DATABASE_URL,
+    sslmode='require'
+)
+conn.set_session(autocommit=True)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# For first use
+try:
+    # Create a DB named "users"
+    cur.execute("CREATE TABLE users (user_id serial PRIMARY KEY, username VARCHAR ( 50 ) UNIQUE NOT NULL);")
+    # Insert a dummy user
+    cur.execute("INSERT INTO users (user_id, username) VALUES (1, 'Heroku');")
+    # Commit all changes
+    conn.commit()
+except Exception as e:
+    print("Table is already exists...")
+
 BASE_HTML = '''<!DOCTYPE html>
 <html>
    <body>
@@ -14,6 +41,13 @@ BASE_HTML = '''<!DOCTYPE html>
 @app.route("/")
 def index():
     return BASE_HTML
+
+
+@app.route("/users")
+def users():
+    cur.execute("SELECT * FROM users;")
+    user = cur.fetchone()
+    return f"User ID: {user[0]}\nUser Name: {user[1]}"
 
 
 if __name__ == '__main__':
